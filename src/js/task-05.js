1798d2b35ce4d9b245e0ceda9b63c03db759b0e4
IvanGrabovskyi <125782890+IvanGrabovskyi@users.noreply.github.com>
2023-07-02 11:56:18 +0800

    first try make hw

---
+++ src/js/task-05.js
@@ -0,0 +1,11 @@
+const textInput = document.querySelector("#name-input");
+const textOutput = document.querySelector("#name-output");
+textInput.addEventListener("input", handleInput);
+function handleInput() {
+  if (textInput.value === "") {
+    textOutput.textContent = "Anonymous";
+  } else {
+    textInput.textContent = "Anonymous";
+  }
+  textOutput.textContent = textInput.value;
+}
